ed19658d6710f1a79d049d37fd688a6eebe3c8b3
Cullen Smith <cullensmi@gmail.com>
2020-09-24 18:37:27 -0400

    added dark theme context

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { BrowserRouter} from 'react-router-dom';
 import AppRouter from './AppRouter';
 
+import { ThemeProvider } from '../hooks/ThemeContext';
+
 function App() {
-  return (
-    <div>
-        <BrowserRouter>
+
+  const [theme, setTheme] = useState(true);
+
+  useEffect( () => {
+console.log(theme)
+
+  }, [theme])
+
+  return ( 
+    <div>    
+        <BrowserRouter>  
+        <ThemeProvider>
           <AppRouter />
+          </ThemeProvider>     
       </BrowserRouter>     
     </div>    
   );
