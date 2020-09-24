7bc8b3e8982926cd28d44391eb8445b251210155
Cullen Smith <cullensmi@gmail.com>
2020-09-24 18:37:50 -0400

    added dark theme context

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import Button from './Button';
 
+import {useTheme} from '../hooks/ThemeContext'
+
 export default function Home() {
 
     const loginLink = window.location.origin  + '/login';
     const regLink = window.location.origin  + '/register';
 
+    // const [theme] = useTheme()
+
     return (
+      
         <div>
             <h1>
                 Welcome To Our Service
             </h1>
+            <h2>
+                { useTheme() ? 'Light mode' : 'darkmode'}
+            </h2>
             <br/>
-
             <Button
             onClick = {() => {window.location = loginLink}}
             text='Login button'
             style={{color: 'white', backgroundColor: 'black'}}
-
             />
-          
-
             <br/>
             <Button
             onClick = {() => {window.location = regLink}}
             text='Register button'
-
             />
-         
-
+            
         </div>
     )
-
 }
 
