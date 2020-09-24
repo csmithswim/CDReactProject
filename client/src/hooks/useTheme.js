e6c8171c940290f5241138b36ca67d6da7da974a
Cullen Smith <cullensmi@gmail.com>
2020-09-24 18:36:42 -0400

    added dark theme context

---
+++ client/src/hooks/useTheme.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import {get, set} from '../utils/localStorage';
+
+export default (initialVal) => {
+
+    const [theme, setTheme] = useState( () => {        
+
+    return initialVal;
+})
+
+    useEffect( () => {
+        console.log('Theme changed to: ', theme);
+
+        set('theme', theme)
+    }, [theme])
+
+    return [theme, setTheme]
+
+}
+
